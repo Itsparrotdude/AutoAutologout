71e73f934f40da913cb70c2e50cc7267adc85ed5
Itsparrotdude <noah.volante@gmail.com>
2023-08-07 13:17:59 +1200

    Changed to check targutURL contains

--- js/content.js
+++ js/content.js
@@ -1,10 +1,10 @@
 // content.js
 
-// Define the URL you want to reload
-const targetUrl = 'https://login.syd-1.linewize.net/logout?url=autologout.linewize.net&deviceid=mtalbert.tts&ip=10.100.16.156&mac=10:b5:88:68:cb:4d&&t=1691364646&h=704f19308df72a75e4248ec8867f029b843e9f70';
+// Define part of the URL you want to match
+const targetUrlPart = 'autologout.linewize.net';
 
-// Check if the current tab's URL matches the target URL
-if (window.location.href === targetUrl) {
-  // Send a message to the background script indicating that the tab should be reloaded
+// Check if the current tab's URL contains the target URL part
+if (window.location.href.includes(targetUrlPart)) {
+  // Send a message to the background service worker indicating that the tab should be reloaded
   chrome.runtime.sendMessage({ reloadTab: true });
 }
