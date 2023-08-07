bb17b18a4ff796675f504f968647c7fabb6acc6e
Itsparrotdude <noah.volante@gmail.com>
2023-08-07 22:40:33 +1200

    adding custom reload interval

--- background.js
+++ background.js
@@ -10,3 +10,31 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       }, 10000);
     }
   }); 
+
+
+// background.js updated for user input controlled reload interval
+
+// Define the URL you want to reload
+const targetUrl = 'https://login.syd-1.linewize.net/logout?url=autologout.linewize.net&deviceid=mtalbert.tts&ip=10.100.16.156&mac=10:b5:88:68:cb:4d&&t=1691368456&h=85e839d8b5931aa5332b0ceda66de814217d8152';
+
+// Function to reload the tab with a delay
+function reloadTabWithDelay(tabId, delay) {
+  setTimeout(() => {
+    chrome.tabs.reload(tabId);
+    console.log("Reloading tab as requested...");
+    reloadTabWithDelay(tabId, delay); // Repeat the reload with the same delay
+  }, delay);
+}
+
+// Listen for the tab being updated (e.g., URL change)
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.url && tab.url.includes(targetUrl)) {
+    const delayKey = 'reloadDelay';
+    chrome.storage.local.get(delayKey, (result) => {
+      const delay = result[delayKey] || 10000; // Default to 10 seconds if not set
+      reloadTabWithDelay(tabId, delay);
+    });
+  }
+});
+
+
--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,4 @@
-    // popup.js
+// popup.js
 
 // Function to get the current enable/disable state from storage
 function getExtensionState(callback) {
@@ -21,7 +21,7 @@ function getExtensionState(callback) {
   // Function to update the button text based on the extension state
   function updateButton(enabled) {
     const button = document.getElementById('toggleExtension');
-    button.textContent = enabled ? 'Disable' : 'Enable';
+    button.checked = enabled;
   }
   
   // Initialize the button state when the popup is opened
@@ -29,9 +29,47 @@ function getExtensionState(callback) {
     getExtensionState((enabled) => {
       updateButton(enabled);
     });
-    
-    // Add a click event listener to the button
-    const button = document.getElementById('toggleExtension');
-    button.addEventListener('click', toggleExtensionState);
-  });
   
+    // Add a change event listener to the checkbox
+    const checkbox = document.getElementById('toggleExtension');
+    checkbox.addEventListener('change', toggleExtensionState);
+  });
+
+  
+  // popup.js
+
+// Function to open the GitHub link in a new tab
+function openGitHubLink() {
+    chrome.tabs.create({ url: 'https://github.com/Itsparrotdude/AutoAutologout' });
+  }
+  
+  // Initialize the link click event
+  document.addEventListener('DOMContentLoaded', () => {
+    const link = document.querySelector('.foe-link');
+    link.addEventListener('click', openGitHubLink);
+  });
+
+  function getReloadInterval() {
+    const intervalInput = document.getElementById('reloadInterval');
+    const intervalInSeconds = parseInt(intervalInput.value);
+    return intervalInSeconds * 1000; // Convert to milliseconds
+  }
+  
+  // Function to reload the tab with the user-selected delay
+  function reloadTabWithDelay(tabId, delay) {
+    setTimeout(() => {
+      chrome.tabs.reload(tabId);
+      console.log("Reloading tab as requested...");
+      reloadTabWithDelay(tabId, delay); // Repeat the reload with the same delay
+    }, delay);
+  }
+  
+  // Initialize the reload interval when the popup is opened
+  document.addEventListener('DOMContentLoaded', () => {
+    const intervalInput = document.getElementById('reloadInterval');
+    intervalInput.addEventListener('change', () => {
+      const delay = getReloadInterval();
+      chrome.storage.local.set({ reloadDelay: delay });
+    });
+
+});
